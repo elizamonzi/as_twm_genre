6320ef0ba342770e78e945b0879fd5392684a590
elizamonzi <emonzi1@lsu.edu>
2018-05-27 16:43:54 -0500

    basically updated everything

--- analysis/Analysis.R
+++ analysis/Analysis.R
@@ -2,80 +2,223 @@
 # Draft of Analysis
 #--------------------------------------------------
 library(data.table)
+library(psych)
+library(stringr)
 library(ggplot2)
-library(lavaan)
-library(semPlot)
+library(apaTables)
+install.packages("apaTables")
 #--------------------------------------------------
-semdata <- fread("data/aggregated_data/AnalysisData-Deletion.csv")
+asData <- fread("data/aggregated_data/Master-Data_ICMPC18_May26-withstrats.csv")
 
-names(semdata)
+# Divide Musicians and Non-Musicans 
+names(asData)
 
-analysis_data <- semdata[, .(subjectNo,wmc,gf, GENERAL, ACTIVE, PERCEPTUAL, MUSICAL, SINGING, EMOTIONS,
-                             ravenSex, BeatPerception, MelodicMemory, AdjustedNumberSeries, RavensAvg,
-                             MeanOspanPartialScore, MeanSspanPartialScore, TonePartial)]
+asData[,. (Counts = .N), by ="Source.x"]
 
-analysis_data
+# Defined musician status based on subject pool recruitment 
 
-# Do it like paper OR do it with individual items? 
-# GENERAL  =~ X2 + X6 + X7 + X9 + X11 + X18 +  X19 + X20 + X21 +  X22 + X25 + X26 + X27 + X28 + X29 + X30 + X32 + X33
-2 + 38
-adderVector <- c(2,6,7,9,11,18,19,20,21,25,26,27,28,29,30,32,33)
-forMeasurementModel <- adderVector + 38
-forMeasurementModel
-
-#--------------------------------------------------
-# Create Measurment Model 
-str(semdata)
-## Define Model 
-measurement.model <- '
-gf =~ AdjustedNumberSeries + RavensAvg
-wmc =~ MeanOspanPartialScore + MeanSspanPartialScore + TonePartial
-gen =~ V40 + V44 + V45 + V47 + V49 + V56 + V57 + V58 + V59 + V63 + V64 + V65 + V66 + V67 + V68 + V70 + V71
-'
-
-## lavaan
-measurment.model.fit <- sem(measurement.model, data = semdata, sample.nobs = 239, std.lv = FALSE)
-semPaths(measurement.model.fit, whatLabels = "std", edge.label.cex = 2)
-summary(measurment.model.fit)
-fitMeasures(measurment.model.fit)
-
-semPaths(measurment.model.fit,
-         layout = "tree2",
-         residuals = FALSE,
-         allVars = FALSE, 
-         intercepts = FALSE, 
-         whatLabels = "std", 
-         thresholds = FALSE, 
-         nDigits = 2,
-         rotation = 2)
-
-#--------------------------------------------------
-# Double Models - One with General Predict Beat and Melodic
-#               - Two with Cognitive Predict Beat and Melodic 
-#--------------------------------------------------
-
-# GENERAL PREDICTS BEAT AND MELODIC 
-model.1 <- '
-MelodicMemory ~ V40 + V44 + V45 + V47 + V49 + V56 + V57 + V58 + V59 + V63 + V64 + V65 + V66 + V67 + V68 + V70 + V71
-BeatPerception ~ V40 + V44 + V45 + V47 + V49 + V56 + V57 + V58 + V59 + V63 + V64 + V65 + V66 + V67 + V68 + V70 + V71
-## Covariances
-MelodicMemory ~~ BeatPerception
-'
+# Take String and split it on the comma, as many as there are
+# This creates something call a List 
+asData$stratz <- str_split(string = asData$summary,pattern = ",",n = Inf)
 
 
-## Fits
-model.fit.1 <- sem(model.1, data = semdata, sample.nobs = 239, std.lv = TRUE)
-semPaths(model.fit.1, whatLabels = "std", edge.label.cex = 2)
-summary(model.fit.1)
-fitMeasures(model.fit.1)
+musicalpeople <- asData[Source.x == "Musician"]$stratz
+nonmusicalpeople <- asData[Source.x == "Sona"]$stratz
 
-semPaths(model.fit.1,
-         layout = "tree2",
-         residuals = FALSE,
-         allVars = FALSE, 
-         intercepts = FALSE, 
-         whatLabels = "std", 
-         thresholds = FALSE, 
-         nDigits = 2,
-         rotation = 2)
+musicalvector <- unlist(musicalpeople)
+unmusicalvector <- unlist(nonmusicalpeople)
 
+musicalvector <- str_trim(musicalvector, side ="both")
+unmusicalvector <- str_trim(unmusicalvector, side ="both")
+
+musicalvector <- data.table(musicalvector)
+musicalvector[musicalvector == "rehearsal"] <- "Rehearsal"
+musicalcounts <- musicalvector[, (Counts = .N), by="musicalvector"]
+musicalcounts
+
+unmusicalvector <- data.table(unmusicalvector)
+unmusicalcounts <- unmusicalvector[, (Counts = .N), by="unmusicalvector"]
+unmusicalcounts
+
+setnames(musicalcounts,"V1","Counts")
+setnames(unmusicalcounts,"V1","Counts")
+
+musicalcounts$Source <- "Musical"
+unmusicalcounts$Source <- "Non-Musical"
+
+setnames(musicalcounts,"musicalvector","Strategy")
+setnames(unmusicalcounts,"unmusicalvector","Strategy")
+musicalcounts
+unmusicalcounts
+
+setwd("figures/")
+
+fwrite(musicalcounts, "MusicalCounts.csv")
+fwrite(unmusicalcounts, "UnMusicalCounts.csv")
+
+stratPlotData <- fread("../data/figureData/StrategyPlotData.csv")
+
+nonMusicalResponseAval <- 114 - 22 
+MusicalResponseAval <- 100 - 66 
+
+stratPlotData$TotalResponseAval <- 999
+stratPlotData
+stratPlotData[Source =="Non-Musical"]$TotalResponseAval <- nonMusicalResponseAval
+stratPlotData[Source =="Musical"]$TotalResponseAval <- MusicalResponseAval
+stratPlotData$PercentResponse <- stratPlotData$Counts / stratPlotData$TotalResponseAval * 100
+stratPlotData
+
+table(stratPlotData$Strategy)
+
+str(stratPlotData)
+
+stratPlotData$Strategy <- factor(stratPlotData$Strategy, levels = c(
+  "Rehearsal",
+  "Ordinal", 
+  "No Strategy Indicated", 
+  "Sensory-Motor",
+  "Explicit Musical Word", 
+  "Metaphor", 
+  "Auditory Image",
+  "No Response Available"
+)
+)
+
+setnames(stratPlotData,"Source","Group")
+
+stratPlotData[Group == "Non-Musical"]$Group <- "Non Musician"
+stratPlotData[Group == "Musical"]$Group <- "Musician"
+
+
+ggplot(stratPlotData[Strategy != "No Response Available"], 
+       aes(x = Strategy, y = PercentResponse)) +
+  #      scale_x_discrete(limit = positions) +
+  geom_bar(aes(fill=Group),position = "dodge",stat = "identity") +
+  labs(title = "Tone Strategies Used by Musicians and Non-Musicians", x = "Strategy", y = "Percent Response")
+
+#----------------------------------------------
+# 
+
+
+#----------------------------------------------
+# Predict Tone Score from Number Strats used
+# number of strats used
+names(asData)
+
+asData$Response <- str_detect(asData$summary, pattern = "[A-Z]")
+
+asData$CommaCount <- 0
+
+asData$CommaCountX <- str_count(asData$summary, ",")
+
+asData$StratCount <- ifelse(test = asData$Response == TRUE, yes = asData$CommaCountX + 1, no = 0)
+
+regressionModel <- asData[, .(TonePartial, StratCount, ASI, ASII, ASIII, ASIV, FMT, MTI, MTII, MTIII, MTIV,
+                              melodicTraning, melodicCourses, melodicPrivate, 
+                              harmonicTraning, harmonicCourses, harmonicPrivate,
+                              sightsingTraning, sightsingCourses, sightsingPrivate,
+                              classical, relgious, pop, GENERAL, MUSICAL, BeatPerception, MelodicMemory)]
+
+
+# Replace all Ps in AS and MT 
+
+asData[ASI=="P"]$ASI <- 4
+asData[,ASI := as.numeric(ASI)]
+
+asData$MTI
+asData[ASII=="P"]$ASII <- 4
+asData[,ASII := as.numeric(ASII)]
+
+asData[ASIII=="P"]$ASIII <- 4
+asData[,ASIII := as.numeric(ASIII)]
+
+asData[ASIV=="P"]$ASIV <- 4
+asData[,ASIV := as.numeric(ASIV)]
+
+asData[FMT=="P"]$FMT <- 4
+asData[,FMT := as.numeric(FMT)]
+
+asData[MTI=="P"]$MTI <- 4
+asData[,MTI := as.numeric(MTI)]
+
+asData[MTII=="P"]$MTII <- 4
+asData[,MTII := as.numeric(MTII)]
+
+asData[MTIII=="P"]$MTIII <- 4
+asData[,MTIII := as.numeric(MTIII)]
+
+asData[MTIV=="P"]$MTIV <- 4
+asData[,MTIV := as.numeric(MTIV)]
+
+asData[, .(ASI, ASII, ASIII, ASIV, MTI, MTII, MTIII, MTIV, FMT)]
+
+# Based on scoring algorithm, add to create new composite scores
+asData$CompositeAural <- apply(cbind(asData$ASI , asData$ASII , asData$ASIII , asData$ASIV),1,sum,na.rm=TRUE)
+
+asData$CompositeGrade <- apply(X = cbind(asData$ASI , asData$ASII , asData$ASIII , asData$ASIV ,
+                asData$MTI , asData$MTII , asData$MTIII , asData$MTIV , asData$FMT),1,sum,na.rm=TRUE)
+
+
+regressionModel$TonePartial
+regressionModel$MTI
+names(asData)
+pairs.panels(asData[Source.x == "Musician",.(TonePartial, StratCount, CompositeGrade)], lm = TRUE, stars = TRUE)
+
+pairs.panels(asData[, .(TonePartial, MeanOspanPartialScore, MeanSspanPartialScore)], lm = TRUE, stars = TRUE)
+
+model.1 <- lm(TonePartial ~ GENERAL, data = asData)
+model.2 <- lm(TonePartial ~ GENERAL + CompositeAural, data = asData)
+model.3 <- lm(TonePartial ~ GENERAL + CompositeAural + MeanSspanPartialScore, data = asData)
+
+summary(model.1 )
+summary(model.2)
+summary(model.3)
+
+apaTables::apa.reg.table(model.1)
+
+plot(model.2)
+
+#----------------------------------
+# Predict AS Grades from ToneSpan Score
+
+pairs.panels(asData[Source.x == "Musician", .(TonePartial, CompositeAural, CompositeGrade)], lm = TRUE, stars = TRUE)
+
+
+
+#-----------------------------------
+# Exploratory Part 
+names(asData)
+
+asData$refComp <- apply(cbind(asData$classical, asData$blues, asData$folk, asData$jazz),1,sum)
+asData$intReb <- apply(cbind(asData$alternative, asData$rock, asData$heavyMetal),1,sum)
+asData$upConv <- apply(cbind(asData$country, asData$relgious, asData$pop, asData$SoundtracksThemeSongs),1,sum)
+asData$energRhyth <- apply(cbind(asData$dancElectronica, asData$rapHipHop, asData$soulFunk),1,sum)
+
+asData$hypTonal <- apply(cbind(asData$classical, asData$soulFunk, asData$relgious, asData$pop),1,sum)
+asData$hypAtonal <- apply(cbind(asData$rapHipHop, asData$heavyMetal, asData$dancElectronica),1,sum)
+
+asData$instrument
+
+
+pairs.panels(asData[Source.x == "Musician", .(hypTonal, hypAtonal, CompositeAural)], lm = TRUE, stars = TRUE)
+
+pairs.panels(asData[Source.x == "Musician", .(refComp, intReb, upConv, energRhyth, CompositeAural)], lm = TRUE, stars = TRUE)
+
+pairs.panels(asData[Source.x == "Musician", .(classical, 
+                        blues, 
+                        country, 
+                        dancElectronica, 
+                        folk,
+                        rapHipHop, 
+                        soulFunk, 
+                        relgious, 
+                        alternative, 
+                        jazz, 
+                        rock, 
+                        pop, 
+                        heavyMetal, 
+                        SoundtracksThemeSongs, 
+                        CompositeAural)], lm = TRUE, stars = TRUE)
+
+ggplot(asData[Source.x == "Musician"], aes(x = CompositeAural, y = heavyMetal, size = GENERAL)) + 
+  geom_point() + geom_jitter() + geom_smooth(method = "lm")
--- scripts/BigKahuna.R
+++ scripts/BigKahuna.R
@@ -22,18 +22,18 @@ file.remove(junk)
 setwd("../../..")
 #--------------------------------------------------
 # Create Number Series Data
-#--------------------------------------------------
-setwd("data/numberseries/combined/")
-# Create New Rotation Individual Files 
-score.number.series()
-# Update New Dataset 
-create.num.dataset()
-# Do we get warnings?!?!?
-# Remove Junk Files
-junk <- dir(pattern="num_data") 
-file.remove(junk)
-setwd("../../..")
-#--------------------------------------------------
+# #--------------------------------------------------
+# setwd("data/numberseries/combined/")
+# # Create New Rotation Individual Files 
+# score.number.series()
+# # Update New Dataset 
+# create.num.dataset()
+# # Do we get warnings?!?!?
+# # Remove Junk Files
+# junk <- dir(pattern="num_data") 
+# file.remove(junk)
+# setwd("../../..")
+# #--------------------------------------------------
 # Score Ospan
 #--------------------------------------------------
 setwd("data/ospan/combined/")
@@ -96,6 +96,37 @@ setwd("data/qualtrics/combined")
 create.qualtrics()
 setwd("../../..")
 #--------------------------------------------------
+# Merge on Strats
+setwd("data/tonespan/strategies/")
+tone_span_data_w_summary <- read_excel("tone_span_data_w_summary_EE.xls")
+setnames(tone_span_data_w_summary, "subject_id","jsPsychId")
+raw_tone_span_strats <- read.csv("cleanTSpanResponse1.csv")
+tone_span_data_w_summary <- data.table(tone_span_data_w_summary)
+raw_tone_span_strats <- data.table(raw_tone_span_strats)
+setnames(raw_tone_span_strats,c("X","Subject"),c("jsPsychId","subjectNo"))
+#-------------------------------
+# Make Strat joined table 
+str(tone_span_data_w_summary)
+raw_tone_span_strats$jsPsychId <- as.character(raw_tone_span_strats$jsPsychId)
+
+joinedSubject <- Reduce(function(x, y) merge(x, y, all=TRUE), 
+                      list(tone_span_data_w_summary, raw_tone_span_strats))
+
+joinedSubject$subjectNo <- as.character(joinedSubject$subjectNo)
+joinedSubject$Strata <- as.character(joinedSubject$Strata)
+
+joinedSubjectSmall <- joinedSubject[,.(subjectNo,jsPsychId, summary, Strata)]
+setnames(joinedSubjectSmall, "subjectNo","subjectNo")
+
+wm_grades <- fread("Participant Log and Notes - Sheet1.csv")
+str(wm_grades)
+setnames(wm_grades, "Participant Number", "subjectNo")
+wm_grades$subjectNo <- as.character(wm_grades$subjectNo)
+
+setwd("../../..")
+
+# strats as needed are just here, put in different folder eventually 
+#--------------------------------------------------
 # Create Master Analysis Spreadheet
 #--------------------------------------------------
 # Create Master Spreadsheet for Analysis 
@@ -127,10 +158,15 @@ setnames(subjectLog,"Participant Number","subjectNo")
 masterMay14 <- Reduce(function(x, y) merge(x, y, all=TRUE), 
                        list(goldExtraData,beatDataMay14,melodicDatayMay14,
                             ravensDataMay14,numberSeriesDataMay14,
-                            wmcDataMay14,ravensData,subjectLog))
+                            wmcDataMay14,ravensData,subjectLog, wm_grades))
+
+withstrats <- masterMay14[joinedSubjectSmall, on = "subjectNo"]
+
+fwrite(withstrats,"Master-Data_ICMPC18_May26-withstrats.csv")
+
 
 fwrite(masterMay14,"Master-Data_ICMPC18_May14.csv")
-# Drop V gold, ravens, op, sym, tone
+     # Drop V gold, ravens, op, sym, tone
 
 goldExtraData[, V1 := NULL]
 ravensData[, V1 := NULL]
